87eafa3f9c68bb34ceaebcea932f52b792dab939
nakayama <nakayama@urbanresearch.co.jp>
2024-03-18 22:34:23 +0900

    add react-router-dom

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 //import App from './App';
 import ChatApp from './ChatApp';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChatApp />
+    <Router basename={process.env.PUBLIC_URL}>
+      <ChatApp />
+    </Router>
   </React.StrictMode>
 );
 
